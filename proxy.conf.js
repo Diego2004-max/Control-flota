f6646e6350d93e0eb5796bb5b4649cd65898b7d0
maajjjoo <majose2006@gmail.com>
2026-09-15 20:24:22 -0500

    arreglo json

---
+++ proxy.conf.js
@@ -0,0 +1,1 @@
+module.exports = { "/": { target: "http://localhost:4200", secure: false, bypass: function (req, res) { res.setHeader("Cross-Origin-Opener-Policy", "same-origin"); res.setHeader("Cross-Origin-Embedder-Policy", "require-corp"); } } }; 
